71735153d7854a02012d0bba30e4cbc312e6bf6e
Azrael <13802880354@139.com>
2019-06-10 22:49:11 +0800

    update .babelrc add antd css

--- src/app.jsx
+++ src/app.jsx
@@ -4,12 +4,14 @@ import { Router } from 'react-router'
 import { Provider } from 'react-redux'
 
 import 'antd-mobile/dist/antd-mobile.css'
+import 'antd/dist/antd.css'
 import '@/Assets/icon/iconfont.css'
 import '@/Assets/sass/main.scss'
 
 import { store, history } from '@/Store/index'
 import Routes from '@/Router/index'
 import { getWxUserInfoSuccess } from '@/Action/wxsign'
+import { actionEnterRoom } from '@/Action/user'
 
 let token = JSON.stringify({
   'userid': '4e1845e0644711e98a720fd6f7c4240e',
@@ -20,6 +22,10 @@ let token = JSON.stringify({
 // let token = localStorage.getItem('token')
 if (token !== null) {
   store.dispatch(getWxUserInfoSuccess(token))
+}
+let room = localStorage.getItem('room_id')
+if (room !== null) {
+  store.dispatch(actionEnterRoom(room))
 }
 
 ReactDOM.render(
